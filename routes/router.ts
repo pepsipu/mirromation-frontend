5479181ec81cbbfc0235cd17ee2414ca4d6609b9
Sammy Hajhamid <pokeso123@hotmail.com>
2020-05-31 19:52:40 -0700

    hooked streaming info to router

--- routes/router.ts
+++ routes/router.ts
@@ -2,8 +2,14 @@ import express from 'express';
 import searchRegister from './search';
 import getTitleRegister from './viewTitle';
 import getEpisodesRegister from './viewEpisodes';
+import streamEpisodeRegister from './streamEpisode';
 
-const routerRegistrars = [searchRegister, getTitleRegister, getEpisodesRegister];
+const routerRegistrars = [
+  searchRegister,
+  getTitleRegister,
+  getEpisodesRegister,
+  streamEpisodeRegister,
+];
 
 const router = express.Router();
 routerRegistrars.forEach((registrar) => registrar(router));
