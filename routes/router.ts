a6e5b0ae8e432b44b75d22a2cd1cfc4f9177cdea
Sammy Hajhamid <pokeso123@hotmail.com>
2020-05-31 19:45:53 -0700

    added route router to serve api services

--- routes/router.ts
+++ routes/router.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import searchRegister from './search';
+import getTitleRegister from './viewTitle';
+import getEpisodesRegister from './viewEpisodes';
 
-const routerRegistrars = [searchRegister];
+const routerRegistrars = [searchRegister, getTitleRegister, getEpisodesRegister];
 
 const router = express.Router();
 routerRegistrars.forEach((registrar) => registrar(router));
